118ef85c3410ca935ce5cc49465fd3fc8af35976
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-06-02 01:24:57 +0200

    NN

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ EventEmitter = require('events'),
 emitter = new EventEmitter();
 emitter.setMaxListeners(Number.POSITIVE_INFINITY);
 
-console.clear();console.log(`${colors.rainbow(figlet.textSync('Dany-LF'))}\n${chalk.blue("=============================================")}\nConfig:\n${chalk.red("Logs Guilds Messages")}: ${chalk.blue(conf.logguildsmsg)}\n${chalk.red("Logs DMs")}: ${chalk.blue(conf.logdmsmsg)}\n${chalk.blue("=============================================")}\nPlease wait 3 seconds.`);
+console.clear();console.log(`${colors.rainbow(figlet.textSync('Hideaki'))}\n${chalk.blue("=============================================")}\nConfig:\n${chalk.red("Logs Guilds Messages")}: ${chalk.blue(conf.logguildsmsg)}\n${chalk.red("Logs DMs")}: ${chalk.blue(conf.logdmsmsg)}\n${chalk.blue("=============================================")}\nPlease wait 3 seconds.`);
 setTimeout(login, 3000);function login(){try{msglogger.login(conf.token)} catch {console.log(chalk.red("INVALID TOKEN!!"));}}
 
 msglogger.on('ready', () => {
